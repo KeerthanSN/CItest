59715025b4ea0341944ea8c61989c2ee7601c86f
Keerthan SN <Keerthan@devopsenabler.com>
2021-06-27 22:07:56 +0530

    first commit

---
+++ BCroad_bud.php
@@ -0,0 +1,14 @@
+ Volume in drive D is New Volume
+ Volume Serial Number is 7094-724D
+
+ Directory of D:\CItest
+
+27-06-2021  22:07    <DIR>          .
+27-06-2021  22:07    <DIR>          ..
+22-06-2021  11:27    <DIR>          .github
+27-06-2021  22:07                 0 BCroad_bud.php
+16-11-2019  14:51             6,663 BCroad_bus.php
+22-06-2021  11:40                85 python.py
+22-06-2021  11:19                 8 README.md
+               4 File(s)          6,756 bytes
+               3 Dir(s)  445,874,130,944 bytes free
